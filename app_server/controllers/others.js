1abd9efc8a3b123f913d66d4e37d5960cb2e2d0d
Chuckz Okoye <tricelex@gmail.com>
2020-02-03 11:09:38 +0100

    updated all view files

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,7 +1,11 @@
 /* GET Homepage */
 
 const about = (req, res) => {
-  res.render('generic-text', { title: 'About' });
+  res.render('generic-text', {
+    title: 'About',
+    content:
+      'Loc8r was created to help people find places to sit down and get a bit of work done.<br/><br/>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem ac nisi digni ssim accumsan. Nullam sit amet interdum magna. Morbi quis faucibus nisi. Vestibulum mollis purus quis eros adipiscing tristique. Proin posuere semper tellus, id placerat augue dapibus ornare. Aenean leo metus, tempus in nisl eget, accumsan interdum dui. Pellentesque sollicitudin volutpat ullamcorper.'
+  });
 };
 
 module.exports = {
